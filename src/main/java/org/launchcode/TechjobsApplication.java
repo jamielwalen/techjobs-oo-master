cffa9381cd59ef9538e6530c9610fc892f7db06d
jamiewhalen <jamielwhalen@gmail.com>
2019-09-05 18:55:35 -0500

    final

--- src/main/java/org/launchcode/TechjobsApplication.java
+++ src/main/java/org/launchcode/TechjobsApplication.java
@@ -3,10 +3,14 @@ package org.launchcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class TechjobsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechjobsApplication.class, args);
 	}
+
+
 }
--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,7 +1,9 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.*;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
+import org.launchcode.models.Job;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 /**
  * Created by LaunchCode
@@ -18,12 +21,16 @@ import javax.validation.Valid;
 public class JobController {
 
     private JobData jobData = JobData.getInstance();
-
+    public Integer jobID;
     // The detail display for a given Job at URLs like /job?id=17
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model, int id) {
+    public String index(Model model, Integer id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+
+        Job someJob = jobData.findById(jobID);
+        model.addAttribute("someJob", someJob);
+
 
         return "job-detail";
     }
@@ -40,8 +47,40 @@ public class JobController {
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Job");
+            return "new-job";
+        }else {
+            String name = jobForm.getName();
+            int empID = jobForm.getEmployerId();
+            int locID = jobForm.getLocationId();
+            int posID = jobForm.getPositionTypeId();
+            int corID = jobForm.getCoreCompetencyId();
 
-        return "";
+
+            Employer aEmployer = jobData.getEmployers().findById(empID);
+
+            Location aLocation = jobData.getLocations().findById(locID);
+
+            PositionType aPositionType = jobData.getPositionTypes().findById(posID);
+
+            CoreCompetency aSkill = jobData.getCoreCompetencies().findById(corID);
+
+            Job newJob = new Job(name, aEmployer, aLocation,
+                    aPositionType, aSkill);
+
+            jobData.add(newJob);
+
+            ArrayList<Job> jobs = jobData.findAll();
+            jobID = jobs.indexOf(newJob);
+            jobID = jobID +1;
+
+            return "redirect:/job";
+        }
+
+
+
+
 
     }
 }
